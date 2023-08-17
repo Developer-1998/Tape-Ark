c76f42160650b004966f75a67d16c58f49af8277
Developer-1998 <mr.sonusangam@gmail.com>
2023-08-17 16:12:15 +0530

    closed navbar

--- tape-ark/src/component/navbar/navbar.jsx
+++ tape-ark/src/component/navbar/navbar.jsx
@@ -66,7 +66,7 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 
 export const Navbar = (props) => {
     const theme = useTheme();
-    const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = React.useState(false);
     const [selectedMenuItem, setSelectedMenuItem] = React.useState(null);
   
 
